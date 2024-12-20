574cff6b325713fd81f78b269c93489c45d46cb1
Robert Funesti <rfunesti@essextech.org>
2024-12-20 09:20:06 -0500

    1

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -2,28 +2,30 @@ using UnityEngine.Events;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EndGame : MonoBehaviour
-{
-    
-    [SerializeField] private string completedText = "GAME OVER!";
-    [SerializeField] private Text completedDisplay;
-
-    public UnityEvent OnGameOver = new UnityEvent();
-
-    /*private void OnEnable()
+namespace TowerDefence {
+    public class EndGame : MonoBehaviour
     {
-        completedDisplay.text = completedText;
-    }*/
 
-    public void CompleteObjective()
-    {
-        if (gameObject.activeSelf)
+        [SerializeField] private string completedText = "GAME OVER!";
+        [SerializeField] private Text completedDisplay;
+
+        public UnityEvent OnGameOver = new UnityEvent();
+
+        /*private void OnEnable()
         {
-            completedDisplay.text = "";
             completedDisplay.text = completedText;
-            OnGameOver.Invoke();
+        }*/
+
+        public void CompleteObjective()
+        {
+            if (gameObject.activeSelf)
+            {
+                completedDisplay.text = "";
+                completedDisplay.text = completedText;
+                OnGameOver.Invoke();
+            }
+
         }
 
     }
-
 }
